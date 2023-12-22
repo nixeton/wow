1a48a956e37d8ecaa103dfaac82ad8cc7fd2fbf8
Sergey Nikiforov <sergey.nikiforov@cenomi.com>
2023-12-22 18:02:09 +0300

    modify config

--- config/config.go
+++ config/config.go
@@ -1,7 +1,6 @@
 package config
 
 import (
-	"fmt"
 	cl "github.com/nixeton/ca"
 	"time"
 )
@@ -9,41 +8,30 @@ import (
 type (
 	// Config -.
 	Config struct {
-		App `yaml:"app"`
 		TCP `yaml:"tpc"`
 		Log `yaml:"logger"`
 		Pow `yaml:"pow"`
 	}
 
-	App struct {
-		Name    string `env-required:"true" yaml:"name"    env:"APP_NAME"`
-		Version string `env-required:"true" yaml:"version" env:"APP_VERSION"`
-	}
-
 	TCP struct {
-		Address   string        `env-required:"true" yaml:"address" env:"TCP_ADDRESS"`
-		KeepAlive time.Duration `env-required:"true" yaml:"keep_alive" env:"TCP_KEEP_ALIVE"`
-		Deadline  time.Duration `env-required:"true" yaml:"deadline" env:"TCP_DEADLINE"`
+		Address   string        `env-required:"true" env:"TCP_ADDRESS"`
+		KeepAlive time.Duration `env-required:"true" env:"TCP_KEEP_ALIVE"`
+		Deadline  time.Duration `env-required:"true" env:"TCP_DEADLINE"`
 	}
 
 	Pow struct {
-		Difficulty int `env-required:"true" yaml:"difficulty" env:"POW_DIFFICULTY"`
+		Difficulty int `env-required:"true" env:"POW_DIFFICULTY"`
 	}
 
 	Log struct {
-		Level string `env-required:"true" yaml:"log_level"   env:"LOG_LEVEL"`
+		Level string `env-required:"true"   env:"LOG_LEVEL"`
 	}
 )
 
 func NewConfig() (*Config, error) {
 	cfg := &Config{}
 
-	err := cl.ReadConfig("./config/config.yml", cfg)
-	if err != nil {
-		return nil, fmt.Errorf("config error: %w", err)
-	}
-
-	err = cl.ReadEnv(cfg)
+	err := cl.ReadEnv(cfg)
 	if err != nil {
 		return nil, err
 	}
